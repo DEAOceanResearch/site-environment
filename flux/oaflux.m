a136aa8e9e7d047e730277d90b6e8a9c9e4266c5
duncombe <>
2012-02-15 17:44:44 +0000

    erica

---
+++ flux/oaflux.m
@@ -0,0 +1,61 @@
+
+print_them=true;
+print_them=false;
+
+morestate=get(0,'more');
+more off
+
+years=[2000:2010];
+dat=[	reshape(ones(12,1)*years,length(years)*12,1), repmat([1:12].',length(years),1), ones(length(years)*12,1)*15 ]; 
+t=datenum(dat);
+
+files= 'evapr_oaflux.nc';
+H(1:3)=plot_oaflux(files,t,'evapr', 'Evaporation rate (cm/year)', 'Evaporation',0.1);
+
+files='lh_oaflux.nc';
+H(end+[1:3])=plot_oaflux(files,t,'lhtfl', 'Monthly Mean (W/m^2)', 'Latent Heat Flux',0.1);
+
+files='sh_oaflux.nc';
+H(end+[1:3])=plot_oaflux(files,t,'shtfl','Monthly Mean (W/m^2)', 'Surface Sensible Heat Flux',0.1);
+
+files='qa_oaflux.nc';
+H(end+[1:3])=plot_oaflux(files,t,'hum2m','Monthly Mean (g/kg)', 'Specific Humidity at 2m',0.01);
+
+files='ws_oaflux.nc';
+H(end+[1:3])=plot_oaflux(files,t,'wnd10','Monthly Mean (m/s)', 'Neutral Wind Speed at 10m',0.01);
+
+
+files='ts_oaflux.nc';
+H(end+[1:3])=plot_oaflux(files,t,'tmpsf','Monthly Mean ( ^\circ{C})', 'Sea Surface Temperature',0.01);
+
+files='ta_oaflux.nc';
+H(end+[1:3])=plot_oaflux(files,t,'tmp2m','Monthly Mean ( ^\circ{C})', 'Air Temperature at 2m',0.01);
+
+% the rest have different time base
+
+years=[1996:2007];
+dat=[	reshape(ones(12,1)*years,length(years)*12,1), repmat([1:12].',length(years),1), ones(length(years)*12,1)*15 ]; 
+t=datenum(dat);
+
+% 1996-2007
+files='qnet.nc';
+H(end+[1:3])=plot_oaflux(files,t,'qnet', 'Monthly Mean (W/m^2)', 'Net Surface Heat Flux',0.1);
+
+files='lw_isccp.nc';
+H(end+[1:3])=plot_oaflux(files,t,'nlwrs', 'Monthly Mean (W/m^2)', 'Net Surface Longwave Radiation Flux',0.1);
+
+files='sw_isccp.nc';
+H(end+[1:3])=plot_oaflux(files,t,'nswrs', 'Monthly Mean (W/m^2)', 'Net Surface Shortwave Radiation Flux',0.1);
+
+
+% want to print them? Use this
+if print_them
+   for i=1:length(H), fig_publish(H(i));
+	print(H(i),'-depsc',sprintf('oafluxfig%02d.eps',i)); 
+   end
+end % print_them
+
+more(morestate);
+
+% vi: se nowrap tw=0 :
+
---
+++ flux/plot_oaflux.m
@@ -0,0 +1,55 @@
+function h=plot_oaflux(filename,t,varname,Ylabel,Title,scl)
+
+if exist('scl','var')~=1, scl=[]; end;
+if isempty(scl), scl=1; end;
+
+% E=loadcdf('evapr_oaflux.nc');
+E=loadcdf(filename);
+
+nyrs=size(E.(varname),4);
+
+% dat=[	reshape(ones(12,1)*[2000:2010],11*12,1), repmat([1:12].',11,1), ones(11*12,1)*15 ];
+
+% t=datenum(dat);
+
+% i=(360-(37:38));
+i=between(wrapTo180(E.lon),[88 90]);
+% j=(90+(25:26));
+j=between(E.lat,[17 19]);
+
+% e=reshape(E.evapr(i,j,:,:),4,12,11);
+e=reshape(E.(varname)(i,j,:,:).*scl,4,12,nyrs);
+
+% time series
+disp([ filename ' ' varname ' ' num2str(minmax(e(:))) ]);
+
+d=datevec(t); 
+
+f=squeeze(mean(e,1));
+h(1)=figure; plot(t,f(:));
+	title(Title)
+	xlabel('Date')
+	ylabel(Ylabel)
+	xaxis(datenum([min(d(:,1)) 1 0;max(d(:,1))+1 1,0]));
+	% set(gca,'xticklabel',['JFMAMJJASOND'].');
+	datetick('keeplimits','keepticks')
+
+% seasonal
+
+h(2)=figure; plot(mean(f.'));
+	title(Title)
+	xlabel('Month')
+	ylabel(Ylabel)
+	axis tight
+	set(gca,'xtick',[1:12],'xticklabel',['JFMAMJJASOND'].');
+
+
+% stats
+
+h(3)=figure; boxplot(f(:));
+	hold on; plot(1,mean(f(:)),'gd','markerfacecolor','g'); 
+	set(gca,'xtick',1,'xticklabel',['Bay of Bengal Site']);
+	ylabel(Ylabel)
+	title(Title)
+
+
