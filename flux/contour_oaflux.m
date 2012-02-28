5e957043fb6a981145e753dd8d1e1cb8050aac33
duncombe <>
2012-02-28 18:06:35 +0000

    grace

--- flux/contour_oaflux.m
+++ flux/contour_oaflux.m
@@ -1,4 +1,9 @@
-function h=contour_oaflux(filename,t,varname,Ylabel,Title,scl,range)
+function [h,co0,co1]=contour_oaflux(filename,varname,yr,Ylabel,Title,scl,range)
+
+% yr is an index to which [month year] to plot
+% range is [W E S N]
+
+if exist('yr','var')~=1, yr=[]; end
 
 if exist('range','var')~=1, error('Provide sampling area'); end
 
@@ -8,52 +13,69 @@ if isempty(scl), scl=1; end;
 % E=loadcdf('evapr_oaflux.nc');
 E=loadcdf(filename);
 
-nyrs=size(E.(varname),4);
+if isempty(yr), yr=[1 size(E.(varname),4)]; end % choose january of last year
+if length(yr)==1, yr=[1 yr]; end % if you give me only a year, use month of january
 
-% dat=[	reshape(ones(12,1)*[2000:2010],11*12,1), repmat([1:12].',11,1), ones(11*12,1)*15 ];
-
-% t=datenum(dat);
+I=find(E.(varname)>32765);
+E.(varname)(I)=NaN;
 
 % i=(360-(37:38));
-i=between(wrapTo180(E.lon),point(1)+[-1 1]);
+i=between(wrapTo180(E.lon),range(1:2));
 % j=(90+(25:26));
-j=between(E.lat,point(2)+[-1 1]);
-
-% e=reshape(E.evapr(i,j,:,:),4,12,11);
+j=between(E.lat,range(3:4));
 
 % keyboard
-e=reshape(E.(varname)(i,j,:,:).*scl,length(i)*length(j),12,nyrs);
+% e=reshape(E.(varname)(i,j,:,:).*scl,length(i)*length(j),12,nyrs);
 
 % time series
-disp([ filename ' ' varname ' ' num2str(minmax(e(:))) ]);
+% disp([ filename ' ' varname ' ' num2str(minmax(e(:))) ]);
 
-d=datevec(t); 
+e=scl.*E.(varname)(i,j,yr(1),yr(2)).';
 
-f=squeeze(mean(e,1));
-h(1)=figure; plot(t,f(:));
-	title(Title)
-	xlabel('Date')
-	ylabel(Ylabel)
-	xaxis(datenum([min(d(:,1)) 1 0;max(d(:,1))+1 1,0]));
+h(1)=figure;
+	m_proj('Mercator','latitudes',range(3:4),'longitudes',range(1:2));
+	[cout,H]=m_contourf(E.lon(i),E.lat(j),e);
+
+% keyboard
+
+	m_coast('speckle');
+	m_grid;
+	title(Title);
+	xlabel(Ylabel);
+	[co0,co1]=ecolorbar(e,'bo');
+	set(co1,'position',[0.13 0.08 0.775 0.04]);
+
+	% colorbar('southoutside');
+	% colorbarf(cout,H,'horiz');
+	% cbarf(e,cout,'horiz','linear');
+
+% d=datevec(t); 
+
+% f=squeeze(mean(e,1));
+% h(1)=figure; plot(t,f(:));
+	% title(Title)
+	% xlabel('Date')
+	% ylabel(Ylabel)
+	% xaxis(datenum([min(d(:,1)) 1 0;max(d(:,1))+1 1,0]));
 	% set(gca,'xticklabel',['JFMAMJJASOND'].');
-	datetick('keeplimits','keepticks')
+	% datetick('keeplimits','keepticks')
 
 % seasonal
 
-h(2)=figure; plot(mean(f.'));
-	title(Title)
-	xlabel('Month')
-	ylabel(Ylabel)
-	axis tight
-	set(gca,'xtick',[1:12],'xticklabel',['JFMAMJJASOND'].');
+% h(2)=figure; plot(mean(f.'));
+	% title(Title)
+	% xlabel('Month')
+	% ylabel(Ylabel)
+	% axis tight
+	% set(gca,'xtick',[1:12],'xticklabel',['JFMAMJJASOND'].');
 
 
 % stats
 
-h(3)=figure; boxplot(f(:));
-	hold on; plot(1,mean(f(:)),'gd','markerfacecolor','g'); 
-	set(gca,'xtick',1,'xticklabel',['Bay of Bengal Site']);
-	ylabel(Ylabel)
-	title(Title)
+% h(3)=figure; boxplot(f(:));
+	% hold on; plot(1,mean(f(:)),'gd','markerfacecolor','g'); 
+	% set(gca,'xtick',1,'xticklabel',['Bay of Bengal Site']);
+	% ylabel(Ylabel)
+	% title(Title)
 
 
---
+++ flux/oaflux_contours.m
@@ -0,0 +1,86 @@
+
+SITE_ENVIRONMENT_DIR='/usr/local/site-environment/flux/';
+RANGE=[75,100,5,25]; 
+print_them=false;
+print_them=true; 
+clear H 
+morestate=get(0,'more');
+more off
+
+tspan=[2000:2010];
+years=[4 find(tspan==2007)];
+% dat=[	reshape(ones(12,1)*years, length(years)*12,1), repmat([1:12].', ...
+	% length(years),1), ones(length(years)*12,1)*15 ]; 
+% t=datenum(dat);
+
+files= [ SITE_ENVIRONMENT_DIR 'evapr_oaflux.nc'];
+
+
+% disp('here')
+
+
+% pause
+
+
+H(1:3)=contour_oaflux(files,'evapr', years, 'Evaporation rate (cm/year)', ...
+		'Evaporation',0.1,RANGE);
+
+files=[ SITE_ENVIRONMENT_DIR 'lh_oaflux.nc'];
+H(end+[1:3])=contour_oaflux(files,'lhtfl', years, 'Monthly Mean (W/m^2)', ...
+		'Latent Heat Flux',0.1,RANGE);
+
+files=[ SITE_ENVIRONMENT_DIR 'sh_oaflux.nc'];
+H(end+[1:3])=contour_oaflux(files,'shtfl', years,'Monthly Mean (W/m^2)', ...
+		'Surface Sensible Heat Flux',0.1,RANGE);
+
+files=[ SITE_ENVIRONMENT_DIR 'qa_oaflux.nc'];
+H(end+[1:3])=contour_oaflux(files,'hum2m', years,'Monthly Mean (g/kg)', ...
+		'Specific Humidity at 2m',0.01,RANGE);
+
+files=[ SITE_ENVIRONMENT_DIR 'ws_oaflux.nc'];
+H(end+[1:3])=contour_oaflux(files,'wnd10', years,'Monthly Mean (m/s)', ...
+		'Neutral Wind Speed at 10m',0.01,RANGE);
+
+
+files=[ SITE_ENVIRONMENT_DIR 'ts_oaflux.nc'];
+H(end+[1:3])=contour_oaflux(files,'tmpsf', years,'Monthly Mean ( ^\circ{C})', ...
+		'Sea Surface Temperature',0.01,RANGE);
+
+files=[ SITE_ENVIRONMENT_DIR 'ta_oaflux.nc'];
+H(end+[1:3])=contour_oaflux(files,'tmp2m', years,'Monthly Mean ( ^\circ{C})', ...
+		'Air Temperature at 2m',0.01,RANGE);
+
+% the rest have different time base
+
+tspan=[1996:2007]; 
+years=[4 find(tspan==2007)];
+
+% dat=[	reshape(ones(12,1)*years,length(years)*12,1), ...
+	% repmat([1:12].',length(years),1), ones(length(years)*12,1)*15 ]; 
+% t=datenum(dat);
+
+% 1996-2007
+files=[ SITE_ENVIRONMENT_DIR 'qnet.nc'];
+H(end+[1:3])=contour_oaflux(files,'qnet', years, 'Monthly Mean (W/m^2)', ...
+		'Net Surface Heat Flux',0.1,RANGE);
+
+files=[ SITE_ENVIRONMENT_DIR 'lw_isccp.nc'];
+H(end+[1:3])=contour_oaflux(files,'nlwrs', years, 'Monthly Mean (W/m^2)', ...
+		'Net Surface Longwave Radiation Flux',0.1,RANGE);
+
+files=[ SITE_ENVIRONMENT_DIR 'sw_isccp.nc'];
+H(end+[1:3])=contour_oaflux(files,'nswrs', years, 'Monthly Mean (W/m^2)', ...
+		'Net Surface Shortwave Radiation Flux',0.1,RANGE);
+
+
+% want to print them? Use this
+if print_them
+   for i=1:length(H), fig_publish(H(i));
+	print(H(i),'-depsc',sprintf('oafluxcontours%02d.eps',i)); 
+   end
+end % print_them
+
+more(morestate);
+
+% vi: se nowrap tw=0 :
+
